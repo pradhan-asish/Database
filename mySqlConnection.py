203e60401a472afa945f3540aa96214e612284bf
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-11 22:30:52 +0530

    Sql connection details

--- mySqlConnection.py
+++ mySqlConnection.py
@@ -2,7 +2,9 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="127.0.0.1",
   user="root",
-  password="Pradhan@1994"
+  password="Pradhan@1994",
+  database='test',
+  auth_plugin='mysql_native_password'
 )
 
 print(mydb)
