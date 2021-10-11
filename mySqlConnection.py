831b0bff94f938423c958313d982db2172315353
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-11 22:53:15 +0530

    unable to connect to database

--- mySqlConnection.py
+++ mySqlConnection.py
@@ -3,8 +3,7 @@ mydb = mysql.connector.connect(
   host="127.0.0.1",
   user="root",
   password="Pradhan@1994",
-  database='test',
-  auth_plugin='mysql_native_password'
+  database='p_table' 
 )
 
 print(mydb)
