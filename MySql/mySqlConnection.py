ac7a504e6cd9568a6006f4eba72cc447f2a2b6b2
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-21 23:54:31 +0530

    Connection name established

--- MySql/mySqlConnection.py
+++ MySql/mySqlConnection.py
@@ -1,7 +1,7 @@
 import mysql.connector
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
+  host="127.0.0.1",
+  user="asish",
   password="Pradhan@1994",
   database='p_table' 
 )
