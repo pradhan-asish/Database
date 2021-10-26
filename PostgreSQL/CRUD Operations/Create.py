d87b0b9f4e9e680b20bccf9368f627f55b40d996
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-26 22:39:14 +0530

    Create table file

--- PostgreSQL/CRUD Operations/Create.py
+++ PostgreSQL/CRUD Operations/Create.py
@@ -1,3 +1,13 @@
 import psycopg2
 from psycopg2 import Error
+import Connections as conn
 
+def createTable():
+    
+    try:
+        connections = conn.openConnection()
+        cursor = connection.cursor()
+        conn.openConnection(connections)
+        
+    except (Exception, Error) as error:
+        print("Error while closing the connection to PostgreSQL", error)
