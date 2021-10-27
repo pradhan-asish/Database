8f8124f3c5dcfd07c03b45959d32d302228fc3cf
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-27 11:40:52 +0530

    Read data by id

--- PostgreSQL/CRUD Operations/Read.py
+++ PostgreSQL/CRUD Operations/Read.py
@@ -19,5 +19,23 @@ def readData():
         
     except (Exception, Error) as error:
         print("Error while fetching the data", error)
-
+        
+def readDataById(id):
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="Pradhan@1994",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="mydb")
+        cursor = connection.cursor()
+        sql = '''Select * from Student where id = %s'''
+        cursor.execute(sql,id)
+        record = cursor.fetchall()
+        print(record)
+        
+        connection.close()
+        
+    except (Exception, Error) as error:
+        print("Error while fetching the data", error)
+        
 readData()
