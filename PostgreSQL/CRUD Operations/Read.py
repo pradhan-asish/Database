7fc35e28ca575b2ba5b3543681c2d002a5fccb0d
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-27 09:45:43 +0530

    Connection established

--- PostgreSQL/CRUD Operations/Read.py
+++ PostgreSQL/CRUD Operations/Read.py
@@ -1,1 +1,12 @@
+import psycopg2
+from psycopg2 import Error
 
+def readData():
+    
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="Pradhan@1994",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="mydb")
+        
