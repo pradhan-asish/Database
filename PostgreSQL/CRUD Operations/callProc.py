b30fc99dfd48d95a9921f5247a81f8a5c90858da
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-28 20:44:16 +0530

    connection established

--- PostgreSQL/CRUD Operations/callProc.py
+++ PostgreSQL/CRUD Operations/callProc.py
@@ -9,3 +9,17 @@ import psycopg2
 from psycopg2 import Error
 
 
+def callProc():
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                  password="Pradhan@1994",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="mydb")
+        
+        connection.close()
+        
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        return connection
